0d9dea2e139f74458e9e9d8831ed4122e9923304
Your Name <owenmalcolm743@gmail.com>
2025-09-23 14:50:40 +0100

    Add 676767.py hypotenuse calculator

---
+++ 676767.py
@@ -0,0 +1,10 @@
+import math as fortnite
+import numpy as np
+
+def function():
+    x=int(input("Enter side 1 length: "))
+    y = int(input("Enter side 2 length: "))
+    print(fortnite.sqrt(x**2 + y**2))
+    
+    
+function()
